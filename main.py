d6c903b813d45f8ad1f58e94383b3050ad038068
maurice otieno <mochieng46@gmail.com>
2023-03-08 21:35:13 +0300

    added module.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-languages=["python","kotlin","java","c++"]
-languages[1] = "ruby"	
-print("python" in languages)
+class Vehicle:
+    def __init__(self,model,make,year_manufacture):
+        self.model = model
+        self.make = make
+        self.year_manufacture=year_manufacture
+        
+    def car_details(self):
+        print(self.model,self.make,self.year_manufacture)
+        
+class Saloon_cars(Vehicle):
+    def saloondetails(self):
+        print(self.model)
---
+++ module.py
@@ -0,0 +1,6 @@
+from main import Vehicle,Saloon_cars
+
+
+v1=Saloon_cars("mazda","demio",2010)
+v1.saloondetails()
+v1.car_details()

541590460953ad24b62880df6c86bde63dba488f
maurice otieno <mochieng46@gmail.com>
2023-03-15 12:07:04 +0300

    modified main.py and module.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-class Vehicle:
-    def __init__(self,model,make,year_manufacture):
-        self.model = model
-        self.make = make
-        self.year_manufacture=year_manufacture
-        
-    def car_details(self):
-        print(self.model,self.make,self.year_manufacture)
-        
-class Saloon_cars(Vehicle):
-    def saloondetails(self):
-        print(self.model)
+
--- module.py
+++
@@ -1,6 +0,0 @@
-from main import Vehicle,Saloon_cars
-
-
-v1=Saloon_cars("mazda","demio",2010)
-v1.saloondetails()
-v1.car_details()
